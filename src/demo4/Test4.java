0a197879ace183bcab33d7ebb6498f6b4fcade7d
juanv <juanv@DESKTOP-TT7GUJN>
2023-04-05 22:51:03 -0400

    seventh commit

---
+++ src/demo4/Test4.java
@@ -0,0 +1,7 @@
+package demo4;
+
+public class Test4 {
+	public static void main(String[] args) {
+		System.out.println("noor meridian juan amina mohammad alzubaidi hamza didnt come in norita");
+	}
+}
